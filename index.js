81f733e1d94b3e34be6b9de6df72f7a263ff45db
martinacd08 <casandra123>
2015-06-07 13:25:20 -0700

    cors

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.all('*', function(req, res, next) {
 */
 
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'example.com');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
