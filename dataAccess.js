c6907edd7dd67e90d7b9a42c6a48977e63424018
martinacd08 <casandra123>
2015-06-07 15:00:39 -0700

    timeout service

--- dataAccess.js
+++ dataAccess.js
@@ -1,10 +1,11 @@
 
 mysql   = require('mysql'),
     connectionpool = mysql.createPool({
-        host     : '192.185.4.105',
-        user     : 'rigarcia_proyect',
-        password : 'proyecto',
-        database : 'rigarcia_proyecto'
+        host     		: 	'192.185.4.105',
+        user     		: 	'rigarcia_proyect',
+        password 		: 	'proyecto',
+        database 		: 	'rigarcia_proyecto',
+		connectTimeout	:	'20000'
 
     });
 
